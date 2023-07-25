9c92da06ab2b6b36e095e4c920478768d246a09b
Anish Joshi <anishjoc@gmail.com>
2023-07-24 21:23:51 -0400

    added docker watch

--- apps/playlist/src/App.tsx
+++ apps/playlist/src/App.tsx
@@ -41,7 +41,7 @@ function App() {
         title="Playlist"
         navLinks={[
           {
-            label: "Home",
+            label: "Homes",
             path: "/",
           },
           {
